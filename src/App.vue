6e39904b5e5b021723958faccdc949591ecb0ccb
FilippoBonanno <fbonanno15@gmail.com>
2024-05-29 16:57:33 +0200

    M-2

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,29 @@ export default {
   },
   data() {
     return {
-      store
+      store,
+      selectedOption: ""
+
     }
   },
   methods: {
+    typeSelection() {
+      if (this.selectedOption) {
+
+
+        axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${this.selectedOption}`).then(risultato => {
+          this.store.personaggi = risultato.data.data;
+
+        }).catch (error => {
+          alert("Non Ci sono Carte")
+        });
+        
+      }
+    }
 
   },
   created() {
-    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0").then(risultato => {
-      this.store.personaggi = risultato.data.data;
-
-    });
+    
 
     axios.get("https://db.ygoprodeck.com/api/v7/archetypes.php").then(risultato => {
       this.store.archetype = risultato.data;
@@ -37,11 +49,15 @@ export default {
 
 <template>
   <h1>Yu-Gi-Oh Api</h1>
+
   <label for="mosters">Choose Archetype </label>
 
-  <select name="mosters">
-    <option v-for="name, i in this.store.archetype" value="volvo">{{name.archetype_name}}</option>
+  <select @change="typeSelection" v-model="selectedOption" name="mosters">
+    <option>Seleziona un Tipo di Carte</option>
+    <option v-for="name, i in this.store.archetype">{{ name.archetype_name }}</option>
   </select>
+
+  <h2>Carte Presenti:  {{ this.store.personaggi.length }}</h2>
   <CardList />
 </template>
 
